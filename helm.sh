49b270931868d43d07b0a5d37cd228519c831362
nisainan <taosheng205054@gmail.com>
2024-06-20 15:44:33 +0800

    modify chart

--- helm.sh
+++ helm.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+cd charts
+
+# 对charts目录下的所有子目录执行helm package ./目录
+for dir in */; do
+    if [ -d "$dir" ]; then
+        helm package "./$dir"
+    fi
+done
+
+cd ../
+
 helm repo index --url https://nisainan.github.io/helm-charts/charts ./charts
-mv charts/index.yaml ./
+
+mv charts/index.yaml ./index.yaml
